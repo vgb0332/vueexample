9ac55ce6d62525e15547d45342502e0c18fe3594
Yongsuk Mark Jung <vgb000123@gmail.com>
2019-06-29 17:19:55 +0900

    a

--- chapter-12/petstore/src/firebase.js
+++ chapter-12/petstore/src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase';
 
 const app = initializeApp({
-  apiKey: "AIzaSyCQYbZoUzPWh6mqfTvaIE6CB4MomAlTtSs",
-  authDomain: "vuejsinaction-f1b96.firebaseapp.com",
-  databaseURL: "https://vuejsinaction-f1b96.firebaseio.com",
-  projectId: "vuejsinaction-f1b96",
-  storageBucket: "vuejsinaction-f1b96.appspot.com",
-  messagingSenderId: "596311474103",
-  appId: "1:596311474103:web:68d447e9b982cdba"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 export const db = app.database();
